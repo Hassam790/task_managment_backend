b7e4ee5909889a775323998fae327fc967a4f79e
Hassam790 <hassamkhan7900@gmail.com>
2025-09-10 16:24:17 +0500

    chore: add .gitignore; fix build to ensure dist emission

--- dist/events/entities/event-log.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class EventLog {
-    id: string;
-    eventType: string;
-    taskId: string;
-    userId: string;
-    timestamp: Date;
-}
--- dist/users/users.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UsersService } from './users.service';
-import { UpdateUserDto } from './dto/update-user.dto';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    getMe(user: any): Promise<any>;
-    updateUser(id: string, dto: UpdateUserDto): Promise<import("./entities/user.entity").User>;
-}
